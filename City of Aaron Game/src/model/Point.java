8fd7ad55251f71d59005aff297b6ab106fc7282d
Teresa <moserte@byui.edu>
2018-10-13 09:19:37 -0600

    saving Point

---
+++ City of Aaron Game/src/model/Point.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+/**
+ *
+ * @author teresamoser
+ */
+
+public class Point implements Serializable {
+    private int row;
+    private int column;
+    
+    public Point(){
+        //Empty constructor for bean
+    }
+    public int getRow(){
+        return row;
+    }
+    public void setRow(int row){
+        this.row = row;
+    }
+    public int getColumn(){
+        return column;
+    }
+    public void setColumn(int column){
+        this.column = column;
+    }
+   
+    //toString method
+    @Override
+    public String toString(){ 
+        
+    }
+    
+}

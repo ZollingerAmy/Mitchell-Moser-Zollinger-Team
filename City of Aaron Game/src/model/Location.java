c74e536f76ddfe1aa883429ee0ff82301799c47f
Teresa <moserte@byui.edu>
2018-10-13 18:30:19 -0600

    saving Location

--- City of Aaron Game/src/model/Location.java
+++ City of Aaron Game/src/model/Location.java
@@ -20,8 +20,8 @@ public class Location implements Serializable {
     public Location(){
         //Empty constructor for bean
     }
-    public String getName(){
-        return name();
+    public String getName() {
+        return name;
     }
     public void setName(String name){
         this.name = name;
@@ -29,7 +29,30 @@ public class Location implements Serializable {
     public String getDescription(){
         return description;
     }
-    public String 
+    public void setDescription(String description){
+            this.description = description;
+    }
+    public String getMapSymbol(){
+        return mapSymbol;
+    }
+    public void setMapSymbol(String mapSymbol){
+        this.mapSymbol = mapSymbol;
+    }
+    public String[] getGameTips(){
+        return gameTips;
+    }
+    public void setGameTimps(String[] gameTips){
+        this.gameTips = gameTips;  
+    }
     
     
+    // toString method
+    @Override
+    public String toString(){
+        return "Location:  " + name
+                + "Description:  " + description
+                + "Map Symbol:  " + mapSymbol
+                + "Game Tips:  " + gameTips;             
+    } 
+       
 }

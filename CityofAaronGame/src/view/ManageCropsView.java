7e5aea50c6ed7122a2ee51c1c5e413661755b7c2
Teresa <moserte@byui.edu>
2018-11-07 21:28:26 -0700

    Manage Crops View fix

--- CityofAaronGame/src/view/ManageCropsView.java
+++ CityofAaronGame/src/view/ManageCropsView.java
@@ -1,14 +1,185 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Manage Crops View 
  */
 package view;
+import java.util.Scanner;
+
 
 /**
  *
- * @author Amy
+ * @author TeresaMoser
  */
 public class ManageCropsView {
+      
+    /**
+     * The message that will be displayed by this view.
+     */
+    protected String message;
+    
+    /**
+     * Constructor
+     */
+    public ManageCropsView(){
+        
+        message = "Manage Crops Menu:  \n"
+                + "B - Buy Land \n"
+                + "S - Sell Land \n"
+                + "F - Feed the People\n"
+                + "P - Plant Crops \n"
+                + "T - Pay Tithes and Offereings \n"
+                + "R - Return to the Game Menu \n";               
+    }
+    
+    /**
+     * Get the user's input. Keep prompting them until they enter a value.
+     * @param prompt
+     * @param allowEmpty - determine whether the user can enter no value (just a return key)
+     * @return 
+     */
+    protected String getUserInput(String prompt, boolean allowEmpty){
+        
+        Scanner keyboard = new Scanner(System.in);
+        String input = "";
+        boolean inputReceived = false;
+        
+        while(inputReceived == false){
+            
+            System.out.println(prompt);
+            input = keyboard.nextLine();
+            
+            // Make sure we avoid a null-pointer error.
+            if (input == null){
+                input = "";
+            }
+            
+            // Trim any trailing whitespace, including the carriage return.
+            input = input.trim();
+            
+            if (input.equals("") == false || allowEmpty == true){
+                inputReceived = true;
+            }
+        }
+        
+        return input;
+    }
+    
+    
+    /**
+     * An overloaded version of getUserInput that sets allowEmpty to false so we don't have 
+     * to type it ourselves.
+     * @param prompt
+     * @return 
+     */
+    protected String getUserInput(String prompt){
+        return getUserInput(prompt, false);
+    }
+    
+    /**
+     * Get the set of inputs from the user.
+     * @return 
+     */
+    public String[] getInputs() {
+        
+        // Declare the array to have the number of elements you intend to get 
+        // from the user.
+        String[] inputs = new String[1];
+        
+        inputs[0] = getUserInput("Choose the letter of the next action you want to"
+                + " take from the Menu. On the next line place letter and press enter.");
+        
+        // Repeat for each input you need, putting it into its proper slot in the array.
+        
+        return inputs;
+    }  
+    /**
+     * Perform the action indicated by the user's input.
+     * @param inputs
+     * @return true if the view should repeat itself, and false if the view
+     * should exit and return to the previous view.
+     */
+    public boolean doAction(String[] inputs){
+        // Act on the user's input.
+        // This is a "dispatch" function that decides what
+        // other functions to call. You can use an if-, if-else,
+        // or switch statement.
+        
+        // return false if you want this view to exit and return
+        // to the view that called it.
+        
+        switch(inputs[0].trim().toUpperCase()){
+            case "B":
+                buyLand();
+                break;
+            case "S":
+                sellLand();
+                break;
+            case "F":
+               feedPeople();
+                break;
+            case "P":
+                acresPlanted();
+                break;
+            case "T":
+                tithesPercent();
+                break;
+            case "R":
+               MainMenuView();
+                return false;
+        }
+      return true;
+    }
+  
+    /**
+     * Control this view's display/prompt/action loop until the user
+     * chooses and action that causes this view to close.
+     */
+    public void displayView(){
+        
+        boolean keepGoing = true;
+        
+        while(keepGoing == true){
+            
+            System.out.println(message);
+            String[] inputs = getInputs();
+            keepGoing = doAction(inputs);
+        }
+    }
+    
+    
+    // Define your action handlers here. These are the methods that your doAction()
+    // method will call based on the user's input. We don't want to do a lot of 
+    // complex game stuff in our doAction() method. It will get messy very quickly.
+    
+    
+    private void buyLand(){
+       ManageCropsView view = new ManageCropsView();
+       view.displayView();
+    }
+    
+    private void sellLand(){
+       ManageCropsView view = new ManageCropsView();
+       view.displayView();
+    }
+    
+    private void feedPeople(){
+       StorehouseView view= new StorehouseView();
+       view.displayView();
+    }
+    
+    private void acresPlanted(){
+       ManageCropsView view= new ManageCropsView();
+       view.displayView();
+    }
+    
+    private void tithesPercent(){
+       ManageCropsView view= new ManageCropsView();
+       view.displayView();
+    }   
+    
+    private void MainMenuView(){
+       MainMenuView view= new MainMenuView();
+       view.displayView();
+    } 
     
 }
+

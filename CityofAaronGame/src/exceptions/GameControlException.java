9391108f4aaca67d045252d2bb3584bea8cfacd4
Teresa <moserte@byui.edu>
2018-11-27 22:38:16 -0700

    GameControlException source package created

---
+++ CityofAaronGame/src/exceptions/GameControlException.java
@@ -0,0 +1,30 @@
+
+package exceptions;
+
+/**
+ *
+ * @author teresamoser
+ */
+public class GameControlException extends Exception {
+
+    public GameControlException() {
+    }
+
+    public GameControlException(String message) {
+        super(message);
+    }
+
+    public GameControlException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public GameControlException(Throwable cause) {
+        super(cause);
+    }
+
+    public GameControlException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+  
+    
+}
